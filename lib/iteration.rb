194bacce82711c1e29f66072cdc540fef5fa6d94
Yoichi Nagano <yoichi.michael.nagano@gmail.com>
2020-03-23 20:05:28 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -44,7 +44,6 @@ def total_even_pairs(src)
   row_index = 0
   while row_index < src.length do
     if ( src[row_index][0] % 2 == 0 ) && ( src[row_index][1] % 2 == 0 )
-    #if ( src[row_index][0] + src[row_index][1] ) % 2 == 0
       total += ( src[row_index][0] + src[row_index][1] )
     end
     row_index += 1  
